b1327d6174d5c684eefa4a7a8499d86456c7afb8
Maciej Andrearczyk <maciej.andrearczyk@student.uw.edu.pl>
2014-04-27 12:35:36 +0200

    Poprawki buttonow

--- gala/src/main/gala/activities/MainMenuActivity.java
+++ gala/src/main/gala/activities/MainMenuActivity.java
@@ -54,6 +54,7 @@ public class MainMenuActivity extends Activity {
             @Override
             public void onClick(View view) {
                 viaPhone(view);
+                dialog.hide();
             }
         });
 
