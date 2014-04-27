e0dcffe810e72ee2d3d581fbf6354294842aed87
maciesz <maciekszeszko@gmail.com>
2014-04-27 23:43:05 +0200

    Poprawienie przekazywania danych w rozgrywce CvP.

--- gala/src/main/gala/core/CvPManager.java
+++ gala/src/main/gala/core/CvPManager.java
@@ -26,6 +26,46 @@ public class CvPManager extends AbstractManager {
     }
 
     @Override
+    public void executeSingleMove(Direction direction) {
+        /**
+         * Wykonujemy pojedynczy ruch.
+         * Tak naprawdę pod spodem dzieje się co następuje:
+         * -> krawędź zostaje zaznaczona jako 'odwiedzona'
+         * -> wierzchołek w kierunku direction zostaje zamarkowany jako 'do odbicia'
+         * -> pozycja piłki zostaje zaktualizowana
+         * -> ruch w postaci kierunku trafia na stos ruchów danej kolejki
+         */
+        chart.executeSingleMove(direction);
+
+        /**
+         * Oceń stan rozgrywki.
+         */
+        final GameState gameState = chart.observer.rateActualState();
+
+        /**
+         * Jeśli jesteśmy w stanie akceptującym(patrz enums/GameState), to:
+         * -> zmieniamy kolejkę
+         * -> czyścimy kontener przechowujący dotychczasowe ruchy
+         * -> informujemy widok o zmianie zawodnika
+         */
+        if (gameState == GameState.ACCEPTABLE) {
+            chart.observer.changeTurn();
+            chart.executeMoveSequence();
+            boardView.changePlayer();
+        }
+
+        /**
+         * Ustawiamy stan gry w widoku.
+         */
+        boardView.setGameState(gameState);
+
+        /**
+         * Przekaż widokowi sekwencję ruchów gracza komputerowego,
+         */
+        boardView.drawSequence(getComputerDirectionSeq());
+    }
+
+    @Override
     public List<Direction> getComputerDirectionSeq() {
         List<Direction> resList = ai.executeMoveSequence(chart);
 
@@ -46,6 +86,13 @@ public class CvPManager extends AbstractManager {
          */
         GameState gameState = chart.observer.rateActualState();
         boardView.setGameState(gameState);
+
+        /**
+         * Jeśli komputer zakończy swoją sekwencję na polu akceptującym(patrz enums/GameState),
+         * to zmień zawodnika.
+         */
+        if (gameState == GameState.ACCEPTABLE)
+            boardView.changePlayer();
 
 
         return resList;
